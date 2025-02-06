ad887c692b0ee9d1d9b027bd96fde81ac3af1da1
CodeSpecterX <aulakhharnoor11@gmail.com>
2025-02-06 21:06:31 +1100

    Setting up coding enviornment

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div> LandingPage
+
+      <html>  
+        <body>
+
+          <image></image>
+
+          <h1 id = "AscensionTitle">ASCENSI NN </h1>
+
+          <image></image>
+
+
+
+        </body>     
+      </html>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+        
+  )
+}
