c04bbd2c1dc588cc68b1b8a93a94ca0db3ee0167
Tirth Patel <tirthvpatel2003@gmail.com>
2025-03-10 21:36:01 +1100

    switched to next js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+      <div id = 'BG' style={{height:"100vh"}} >
+
+        <img id = 'Logo' alt = 'Logo' src = {"/logo.png"}/>
+
+        <h1 id = "AscensionTitle">ASCENSI &nbsp; NN </h1>
+
+      </div>     
+      
+  );
+}
