37aca2145b6e591426f11c503c0a486f7f0e153d
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-11 15:47:31 +1000

    added extra stying to review component

--- src/Components/Pages/Review.tsx
+++ src/Components/Pages/Review.tsx
@@ -4,8 +4,15 @@ import {Review as ReviewType} from "@/interfaces/Review";
 export default function Review({testimonial}:{testimonial:ReviewType}){
 
   return (
-    <div className='bg-black w-20 h-20 text-sm'>
-      {testimonial.company}
+    <div className='flex flex-col gap-2 bg-asc-500 text-sm text-white rounded-xl w-[99%] h-40 p-2'>
+      <h1 className='flex text-md font-bold'>{testimonial.title}</h1>
+      <div className='flex w-full h-20 text-zinc-200 text-ellipsis overflow-hidden leading-4.25'>
+        {testimonial.text}
+      </div>
+      <div className='flex flex-col w-full h-10'>  
+        <div className='flex w-full text-sm'>{testimonial.name}</div>
+        <div className='flex w-full text-xs'>{testimonial.company}</div>
+      </div>
     </div>
   )
 }
