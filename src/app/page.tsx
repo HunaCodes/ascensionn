3ab293a0022a33038b422c3b18177b9e9bb8079d
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-13 07:14:12 +1000

    css for snappy scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,19 @@ import Home from "@/Components/Pages/Home";
 import Test from "@/Components/Pages/Test";
 import Review from "@/Components/Pages/Review";
 export default function Ascensionn() {
+  
   const pages = [Home,];
   return (
     <div style={{
       background: "linear-gradient(to bottom, #051923 10%, #003554 44%, #006494 86%, #0582CA 100%)",
-      color:"white",
-      scrollSnapType: "y mandatory",
-      scrollBehavior: "smooth",
-      overflowY: "scroll"
-    }}>
+      // color:"white",
+      // scrollSnapType: "y mandatory",
+      // scrollBehavior: "smooth",
+      // overflowY: "scroll"
+    }}
+      className="text-white snap-y w-screen h-screen overflow-y-auto snap-mandatory"
+    
+    >
       {pages.map((Content, index) => (
         <Page key={index} >
           <Content />
