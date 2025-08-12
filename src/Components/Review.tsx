c170d132783d195f9987aa3678fcca8792c58a47
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-12 13:53:00 +1000

    made the review component fixed height

--- src/Components/Review.tsx
+++ src/Components/Review.tsx
@@ -4,14 +4,14 @@ import {Review as ReviewType} from "@/interfaces/Review";
 export default function Review({testimonial}:{testimonial:ReviewType}){
 
   return (
-    <div className='flex flex-col gap-2 bg-asc-500 text-sm text-white rounded-xl w-[99%] h-40 p-2'>
+    <div className='flex flex-col gap-2 bg-asc-500 text-sm text-white rounded-xl w-[99%] h-[30vh] p-2'>
       <h1 className='flex text-md font-bold'>{testimonial.title}</h1>
-      <div className='flex w-full h-20 text-zinc-200 text-ellipsis overflow-hidden leading-4.25'>
+      <div className='flex w-full h-full relative text-zinc-300 text-ellipsis overflow-hidden leading-4.5'>
         {testimonial.text}
       </div>
-      <div className='flex flex-col w-full h-10'>  
-        <div className='flex w-full text-sm'>{testimonial.name}</div>
-        <div className='flex w-full text-xs'>{testimonial.company}</div>
+      <div className='flex flex-col w-full h-10 font-bold'>  
+        <div className='flex w-full text-sm '>{testimonial.name}</div>
+        <div className='flex w-full text-xs '>{testimonial.company}</div>
       </div>
     </div>
   )
