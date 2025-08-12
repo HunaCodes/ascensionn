3c6bf4f8b9b7bc402123e059e717e68bac8d9730
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-12 15:51:32 +1000

    responsive design landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Image from "next/image";
 
-export default function Home() {
+export default function Ascensionn() {
   return (
-    <div id = 'BG' style={{height:"100vh"}} >
-          <img id = 'Logo' alt = 'Logo' src = {'/logo.png'}/>
-          <h1 id = "AscensionTitle">ASCENSI &nbsp; NN </h1>
-        </div>
+    <div className="bg-[url(/BG2.jpg)] bg-cover w-screen h-screen bg-no-repeat bg-center flex flex-col items-center">
+      <h1 className="flex w-fit text-[2.5rem] sm:text-[5rem] lg:text-[10rem] items-center">ASCENSI 
+        <Image src={'/logo.png'} alt="O" width={1000} height={5000} className="h-10 w-10 sm:h-20 sm:w-20 lg:h-40 lg:w-40 "/> 
+        NN 
+      </h1>      
+    </div>
   );
 }
