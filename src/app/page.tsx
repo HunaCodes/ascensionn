b894587b04e8ac855df6dfbfb226609c0b2b480a
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-15 07:10:38 +1000

    added snappy scrolling for long gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,29 @@
 
 import Page from "@/Components/Page";
 import Home from "@/Components/Pages/Home";
-import Test from "@/Components/Pages/Test";
-import Review from "@/Components/Pages/Review";
+type ComponentConfig = {
+  Component: React.ReactNode
+  props?: any
+}
+
 export default function Ascensionn() {
   
-  const pages = [Home,];
-  return (
-    <div style={{
-      background: "linear-gradient(to bottom, #051923 10%, #003554 44%, #006494 86%, #0582CA 100%)",
-      // color:"white",
-      // scrollSnapType: "y mandatory",
-      // scrollBehavior: "smooth",
-      // overflowY: "scroll"
-    }}
-      className="text-white snap-y w-screen h-screen overflow-y-auto snap-mandatory"
+  const pages:ComponentConfig[] = [
+    {Component: <Home/>},
+    {Component: <Home/>}
     
-    >
-      {pages.map((Content, index) => (
-        <Page key={index} >
-          <Content />
-        </Page>
-      ))}
-      <Page >
-        <Home />
-      </Page>
-      <Page >
-        <Test />
-      </Page>
+  ];
+  return (
+    <div className="text-white snap-y w-screen h-screen overflow-y-auto snap-mandatory">
+      <div style={{
+        background: "linear-gradient(to bottom, #051923 10%, #003554 44%, #006494 86%, #0582CA 100%)"}} 
+      className="flex w-screen h-fit flex-col snap-y snap-mandatory">
+        {pages.map((Content, index) => (
+            <Page key={index} >
+              {Content.Component}
+            </Page>
+        ))}
+      </div>
     </div>
   );
 }
