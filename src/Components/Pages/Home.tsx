b9559fbdb1052249f4178ccb822a560adbc17722
CodeSpecterX <aulakhharnoor11@gmail.com>
2025-09-27 16:38:03 +1000

    Adding home page title and bacground image

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='w-screen h-screen flex flex-col bg-[url("/BG2.jpg")] bg-cover bg-center items-center justify-center'>
+
+      <div className='flex font-bold text-5xl items-center justify-center'>
+        <h1 className='drop-shadow-lg lg:text-8xl'>Ascensi</h1>
+        <Image className="drop-shadow-2xl" src ='/logo.png' alt="Logo" width={90} height={80}></Image>
+        <h1 className='drop-shadow-lg lg:text-8xl'>nn</h1>
+      </div>
+    
+    
+    
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,11 @@ type ComponentConfig = {
 export default function Ascensionn() {
   
   const pages:ComponentConfig[] = [
-    {Component: <Home/>},
     {Component: <Home/>}
     
   ];
   return (
-    <div className="text-white snap-y w-screen h-screen overflow-y-auto snap-mandatory">
+    <div className="text-white snap-y w-screen h-screen overflow-y-auto overflow-hidden snap-mandatory">
       <div style={{
         background: "linear-gradient(to bottom, #051923 10%, #003554 44%, #006494 86%, #0582CA 100%)"}} 
       className="flex w-screen h-fit flex-col snap-y snap-mandatory">
