dda0a2c780893236486dd6cdbe9c9b12d171639a
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-29 15:02:25 +1000

    now using the ReviewPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import Page from "@/Components/Page";
 import Home from "@/Components/Pages/Home";
+import ReviewPage from "@/Components/Pages/ReviewPage";
+
 type ComponentConfig = {
   Component: React.ReactNode
   props?: any
@@ -11,11 +13,11 @@ export default function Ascensionn() {
   
   const pages:ComponentConfig[] = [
     {Component: <Home/>},
-    {Component: <Home/>}
+    {Component: <ReviewPage/>}
     
   ];
   return (
-    <div className="text-white snap-y w-screen h-screen overflow-y-auto snap-mandatory">
+    <div className="text-white snap-y w-screen h-screen overflow-y-auto overflow-x-hidden snap-mandatory">
       <div style={{
         background: "linear-gradient(to bottom, #051923 10%, #003554 44%, #006494 86%, #0582CA 100%)"}} 
       className="flex w-screen h-fit flex-col snap-y snap-mandatory">
