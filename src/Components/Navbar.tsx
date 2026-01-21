0cd4d544459c2efb146005728692629e1e6d78bc
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-01-21 21:53:40 +1100

    Implementation: Navbar menu prototype complete

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   return (
     <div className='absolute h-screen w-screen'>
         
-       <h1 className='h-full w-full flex items-end justify-center pb-10'>
+       <h1 className='h-full w-full flex items-end justify-center pb-10 drop-shadow-lg'>
         
-        <div className='flex w-fit bg-[#051923] rounded-md py-3 px-4 gap-10 text-md'>
+        <div className='flex w-fit bg-[#051923] rounded-full py-3 px-4 gap-10 text-md font-semibold'>
 
-          <button className="cursor-pointer" >About Us</button>
-          <button className="cursor-pointer" >Find Out More</button>
-          <button className="cursor-pointer" >Reviews</button>
-          <button className="cursor-pointer" >FAQs</button>
+          <button className="cursor-pointer bg-[#0F2330] px-4 rounded-full py-2" >About Us</button>
+          <button className="cursor-pointer bg-[#0F2330] px-4 rounded-full py-2" >Find Out More</button>
+          <button className="cursor-pointer bg-[#0F2330] px-4 rounded-full py-2" >Reviews</button>
+          <button className="cursor-pointer bg-[#0F2330] px-4 rounded-full py-2" >FAQs</button>
           <button className="bg-[#00A6FB] px-2 rounded-full py-2 cursor-pointer" >Book Now &rarr;</button>
         
         </div>
