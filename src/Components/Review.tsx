52e167a241f2367c1c8f92001cd9a89af7cb328f
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-27 16:25:19 +1100

    change: changed all fonts to the same size changing only boldness

--- src/Components/Review.tsx
+++ src/Components/Review.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import {Review as ReviewType} from "@/interfaces/Review";
 
 export default function Review({testimonial}:{testimonial:ReviewType}){
-
+  const text = testimonial.text.length> 390? testimonial.text.substring(0,390)+"..." : testimonial.text;
   return (
-    <div className='flex flex-col gap-2 bg-asc-500 text-sm md:text-xl text-white rounded-xl w-full lg:w-[48%] min-h-[25vh] h-fit max-h-[30vh] p-2'>
-      <h1 className='flex flex-row h-fit border-2 text-md font-bold md:text-2xl'>{testimonial.title}</h1>
-      <div className='flex flex-row w-full h-full relative border-2 text-zinc-300 overflow-hidden text-ellipsis leading-4.5'>
-        {testimonial.text}
+    <div className='flex flex-col gap-2 bg-asc-500 text-md text-zinc-200 rounded-xl w-full md:w-[48%] h-[25vh] p-2'>
+      <h1 className='flex flex-row h-fit text-md font-bold'>{testimonial.title}</h1>
+      <div className='flex flex-row w-full h-full relative overflow-hidden text-ellipsis leading-4.5'>
+        {text}
       </div>
-      <div className='flex flex-col w-full h-10 font-bold border-2'>  
-        <div className='flex w-full text-sm md:text-lg '>{testimonial.name}</div>
-        <div className='flex w-full text-xs md:text-md '>{testimonial.company}</div>
+      <div className='flex flex-col w-full h-12 tracking-tighter gap-0 font-bold'>  
+        <div className='flex w-full text-md '>{testimonial.name}</div>
+        <div className='flex w-full text-md '>{testimonial.company}</div>
       </div>
     </div>
   )
