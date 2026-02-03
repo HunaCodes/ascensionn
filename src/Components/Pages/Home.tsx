64e11b087bf79d55a0e549e956a2a08dc2695bc8
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-02-03 22:04:32 +1100

    Implementation: Landing page title new positioning

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className='w-screen h-screen flex flex-col bg-[url("/BG2.jpg")] bg-cover bg-center items-center justify-center'>
 
-      <div className='flex font-bold text-5xl items-center justify-center'>
+      <div className='flex font-bold text-5xl items-center justify-center mb-40'>
         <h1 className='drop-shadow-lg lg:text-8xl'>Ascensi</h1>
         <Image className="drop-shadow-2xl" src ='/logo.png' alt="Logo" width={90} height={80}></Image>
         <h1 className='drop-shadow-lg lg:text-8xl'>nn</h1>
