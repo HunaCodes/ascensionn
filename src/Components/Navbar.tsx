8c47ea9f84299ef2f88926ca17ce30440015db4a
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-02-03 22:15:32 +1100

    Implementation: Further improvement on responsiveness of the landing page, however inconsistencies present under sm breakpoint

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   return (
     <div className='absolute h-screen w-screen'>
         
-       <h1 className='h-full w-full flex items-end justify-center pb-10 drop-shadow-lg'>
+       <h1 className='h-full w-full flex items-end justify-center pb-20 drop-shadow-lg'>
         
-        <div className='flex w-fit bg-[#051923] rounded-full py-3 px-4 gap-10 text-md font-semibold'>
+        <div className='flex w-fit bg-[#051923] rounded-full py-2 px-2 text-xs gap-1 md:py-3 md:px-4 md:gap-5 md:text-lg font-semibold'>
 
-          <button className="cursor-pointer bg-[#0F2330] lg:px-4 rounded-full py-2" >About Us</button>
-          <button className="cursor-pointer bg-[#0F2330] lg:px-4 rounded-full py-2" >Find Out More</button>
-          <button className="cursor-pointer bg-[#0F2330] lg:px-4 rounded-full py-2" >Reviews</button>
-          <button className="cursor-pointer bg-[#0F2330] lg:px-4 rounded-full py-2" >FAQs</button>
+          <button className="cursor-pointer bg-[#0F2330] px-1.5 md:px-3 rounded-full py-2" >Find Out More</button>
+          <button className="cursor-pointer bg-[#0F2330] px-1.5 md:px-3 rounded-full py-2" >About Us</button>
+          <button className="cursor-pointer bg-[#0F2330] px-1.5 md:px-3 rounded-full py-2" >Reviews</button>
+          <button className="cursor-pointer bg-[#0F2330] px-1.5 md:px-3 rounded-full py-2" >FAQs</button>
           <button className="bg-[#00A6FB] px-2 rounded-full lg:py-2 cursor-pointer" >Book Now &rarr;</button>
         
         </div>
