0a7b2a5b140e758c76a2d2cd50d75821e21375e5
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-02-06 01:38:01 +1100

    Implementation: Further landing page enhancements

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     <div className='w-screen h-screen flex flex-col bg-[url("/BG2.jpg")] bg-cover bg-center items-center justify-center'>
 
       <div className='flex font-bold items-center justify-center mb-40'>
-        <h1 className='drop-shadow-lg xs:text-6xl sm:text-7xl md:text-8xl lg:text-[7rem]'>Ascensi</h1>
-        <Image className="drop-shadow-2xl xs:w-17 xs:h-19 sm:w-18 sm:h-20 md:w-22 md:h-25 lg:w-25 lg:h-28" src ='/logo.png' alt="Logo" width={90} height={80}></Image>
-        <h1 className='drop-shadow-lg xs:text-6xl sm:text-7xl md:text-8xl lg:text-[7rem]'>nn</h1>
+        <h1 className='drop-shadow-lg xs:text-6xl sm:text-7xl md:text-8xl lg:text-[6.5rem]'>Ascensi</h1>
+        <Image className="drop-shadow-2xl xs:w-15 xs:h-18 sm:w-18 sm:h-20 md:w-25 md:h-28 lg:w-24 lg:h-28" src ='/logo.png' alt="Logo" width={90} height={80}></Image>
+        <h1 className='drop-shadow-lg xs:text-6xl sm:text-7xl md:text-8xl lg:text-[6.5rem]'>nn</h1>
       </div>
     
     <Navbar></Navbar>
