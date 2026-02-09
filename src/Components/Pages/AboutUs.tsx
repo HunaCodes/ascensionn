dcb3c65a76391c3c664397a5d64a6d6142e525ee
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-02-09 21:25:53 +1100

    Implementaiton: adding an overlay in the about us page for better text clarity

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -5,14 +5,16 @@ export default function AboutUs() {
     <div className='relative w-screen h-screen flex flex-col'>
 
         {/* Background Image */}
-        <div className='w-full h-[45%] top-0 bg-[url("/aboutUsBG.jpeg")] bg-cover bg-center justify-center items-center flex flex-col text-5xl'>
+        <div className='relative w-full h-[45%] top-0 bg-[url("/aboutUsBG.jpeg")] bg-cover bg-center justify-center items-center flex flex-col text-5xl'>
             {/* <div className='bg-gradient-to-b to-transparent from-transparent via-[#051923] from-[5%] via-[15%] w-screen h-80 z-10 absolute top-[40%]'></div> */}
 
+            <div className='absolute inset-0 bg-[#051923] opacity-[70%] z-0'></div>
+
             {/* Title */}
-            <div className='text-white font-bold drop-shadow-lg top-0 h-full mt-5'>About Us</div>
+            <div className='text-white font-bold drop-shadow-lg top-0 h-full mt-5 z-10'>About Us</div>
 
             {/*sBackground information*/}
-            <p className='text-white text-center w-1/2 text-sm mb-30'>At Ascensionn, we help real estate developers boost buyer conversions with our 24/7 AI Sales Concierge. Our system instantly engages, qualifies, and schedules appointments, turning inquiries into closed deals—faster than the competition.</p>
+            <p className='text-white text-center w-1/2 text-sm mb-30 z-10'>At Ascensionn, we help real estate developers boost buyer conversions with our 24/7 AI Sales Concierge. Our system instantly engages, qualifies, and schedules appointments, turning inquiries into closed deals—faster than the competition.</p>
         </div>
 
 
