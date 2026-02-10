3422c46bd81951a5008360d83695dbd627e29de5
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-02-10 23:56:25 +1100

    Implementation: Changing overlay to a gradient from bottom to top

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -10,7 +10,8 @@ export default function AboutUs() {
         {/* Background Image */}
         <div className='relative w-full h-[45%] top-0 bg-[url("/aboutUsBG.jpeg")] bg-cover bg-center justify-center items-center flex flex-col text-5xl'>
 
-            <div className='absolute inset-0 bg-[#051923] opacity-[75%] z-0'></div>
+            {/* Overlay */}
+            <div className='absolute inset-0 bg-gradient-to-t from-[#051923] via-[#051923] to-transparent opacity-[75%] z-0'></div>
 
             {/* Title */}
             <div className='text-white font-bold drop-shadow-lg top-0 h-full mt-5 z-10'>About Us</div>
