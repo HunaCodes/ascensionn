6cdb2f0d253873be6cec0b6547bb995b4c5c3127
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-05-07 17:35:49 +1000

    FIX: Changing business profile positioning form flex-col to flex

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -36,45 +36,35 @@ export default function AboutUs() {
           <h1>Our Team</h1>
           
           {/* Image of the creators */}
-          <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp1(true)}></img>
-          
-          {popup1 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
-            
-            <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp1(false))}>Close</button>            
+          <div className='w-full flex justify-center gap-10'>
+            <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp1(true)}></img>   
+            {popup1 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'> 
+              <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp1(false))}>Close</button>            
+                {/* Personal info */}
+                <div className='w-full items-center font-normal flex flex-col gap-10'>
+                  <h1 className='text-lg'>Gurnoor Singh</h1>
+                  <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
+                </div>
+                {/* Acomplishments */}
+                <div></div>
+                {/* Social Links */}
+                <SocialLinks></SocialLinks>
+            </div>} 
 
-              {/* Personal info */}
-              <div className='w-full items-center font-normal flex flex-col gap-10'>
-                <h1 className='text-lg'>Gurnoor Singh</h1>
-                <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
-              </div>
-
-              {/* Acomplishments */}
-              <div></div>
-
-              {/* Social Links */}
-              <SocialLinks></SocialLinks>
-            
-          </div>} 
-
-          <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp2(true)}></img>
-          
-          {popup2 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
-            
-            <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp2(false))}>Close</button>            
-
-              {/* Personal info */}
-              <div className='w-full items-center font-normal flex flex-col gap-10'>
-                <h1 className='text-lg'>Unknown Business Partner</h1>
-                <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
-              </div>
-
-              {/* Acomplishments */}
-              <div></div>
-
-              {/* Social Links */}
-              <SocialLinks></SocialLinks>
-            
-          </div>} 
+            <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp2(true)}></img>  
+            {popup2 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
+              <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp2(false))}>Close</button>            
+                {/* Personal info */}
+                <div className='w-full items-center font-normal flex flex-col gap-10'>
+                  <h1 className='text-lg'>Unknown Business Partner</h1>
+                  <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
+                </div>
+                {/* Acomplishments */}
+                <div></div>
+                {/* Social Links */}
+                <SocialLinks></SocialLinks>       
+            </div>} 
+          </div>
         
         </div>
 
