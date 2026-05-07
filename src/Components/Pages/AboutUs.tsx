5180c56d8270d5c29d57c252a9983e6885e4639a
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-05-07 17:29:57 +1000

    Implementation: Created a distinct Social Links component for improved code base readabillity

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import SocialLinks from '../PopUpComponents/SocialLinks';
 
 export default function AboutUs() {
 
@@ -50,39 +51,7 @@ export default function AboutUs() {
               <div></div>
 
               {/* Social Links */}
-              <div className='flex gap-5 justify-center w-full h-full mt-20'>
-                    <a href="https://www.instagram.com/yourusername">
-                      <img
-                        src="/instagram.png"
-                        className="w-[50px] h-[50px] cursor-pointer rounded-sm transform hover:scale-90 duration-300"
-                        alt="Instagram"
-                      />
-                    </a>
-
-                    <a href="https://www.youtube.com/@yourchannel">
-                      <img
-                        src="/youtube.png"
-                        className="w-[50px] h-[50px] cursor-pointer transform hover:scale-90 duration-300"
-                        alt="Youtube"
-                      />
-                    </a>
-
-                    <a href="https://www.linkedin.com/company/ascensionn-ecom/posts/?feedView=all">
-                      <img
-                        src="/linkedin.png"
-                        className="w-[50px] h-[50px] cursor-pointer transform hover:scale-90 duration-300"
-                        alt="LinkedIn"
-                      />
-                    </a>
-
-                    <a href="https://x.com/yourusername">
-                      <img
-                        src="/twitter.png"
-                        className="w-[50px] h-[50px] bg-white rounded-md cursor-pointer transform hover:scale-90 duration-300"
-                        alt="X"
-                      />
-                    </a>
-              </div>
+              <SocialLinks></SocialLinks>
             
           </div>} 
         
---
+++ src/Components/PopUpComponents/SocialLinks.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function SocialLinks() {
+  return (
+     <div className='flex gap-5 justify-center w-full h-full mt-20'>
+                    <a href="https://www.instagram.com/yourusername">
+                      <img
+                        src="/instagram.png"
+                        className="w-[50px] h-[50px] cursor-pointer rounded-sm transform hover:scale-90 duration-300"
+                        alt="Instagram"
+                      />
+                    </a>
+
+                    <a href="https://www.youtube.com/@yourchannel">
+                      <img
+                        src="/youtube.png"
+                        className="w-[50px] h-[50px] cursor-pointer transform hover:scale-90 duration-300"
+                        alt="Youtube"
+                      />
+                    </a>
+
+                    <a href="https://www.linkedin.com/company/ascensionn-ecom/posts/?feedView=all">
+                      <img
+                        src="/linkedin.png"
+                        className="w-[50px] h-[50px] cursor-pointer transform hover:scale-90 duration-300"
+                        alt="LinkedIn"
+                      />
+                    </a>
+
+                    <a href="https://x.com/yourusername">
+                      <img
+                        src="/twitter.png"
+                        className="w-[50px] h-[50px] bg-white rounded-md cursor-pointer transform hover:scale-90 duration-300"
+                        alt="X"
+                      />
+                    </a>
+              </div>
+  )
+}
