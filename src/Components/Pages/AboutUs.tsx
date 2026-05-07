1e48195c82a480abf7e8f1006baaca35d9e4647a
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-05-07 17:32:40 +1000

    Implementation: Added an additional business profile

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -6,7 +6,8 @@ export default function AboutUs() {
   const aboutUsText = "At Ascensionn, we specialize in helping Angel Investors attract over 50 thesis-aligned deals within 180 days. Our unique approach empowers investors to build a consistent and proprietary deal flow, ensuring they find high-quality opportunities that match their specific investment goals. With a focus on precision and efficiency, we streamline the deal-sourcing process to create lasting value for both investors and startups.";
   const personalBio = "I'm Gurnoor Singh, founder of Ascensionn. My focus is on helping angel investors find 50+ thesis-aligned deals in just 180 days, while building a steady flow of exclusive opportunities. With a background in real estate and a passion for connecting the right investors to the right deals, I’m dedicated to making the investment process smoother and more efficient."
 
-  const [popup, setPopUp] = useState(false);
+  const [popup1, setPopUp1] = useState(false);
+  const [popup2, setPopUp2] = useState(false);
 
 
   return (
@@ -34,16 +35,36 @@ export default function AboutUs() {
           
           <h1>Our Team</h1>
           
-          {/* Image of the creator */}
-          <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp(true)}></img>
+          {/* Image of the creators */}
+          <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp1(true)}></img>
           
-          {popup === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
+          {popup1 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
             
-            <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp(false))}>Close</button>            
+            <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp1(false))}>Close</button>            
 
               {/* Personal info */}
               <div className='w-full items-center font-normal flex flex-col gap-10'>
                 <h1 className='text-lg'>Gurnoor Singh</h1>
+                <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
+              </div>
+
+              {/* Acomplishments */}
+              <div></div>
+
+              {/* Social Links */}
+              <SocialLinks></SocialLinks>
+            
+          </div>} 
+
+          <img className="w-1/11 transform hover:scale-90 duration-300 cursor-pointer" src="/pp.png" onClick={()=>setPopUp2(true)}></img>
+          
+          {popup2 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'>
+            
+            <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp2(false))}>Close</button>            
+
+              {/* Personal info */}
+              <div className='w-full items-center font-normal flex flex-col gap-10'>
+                <h1 className='text-lg'>Unknown Business Partner</h1>
                 <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
               </div>
 
