d2dbc693c16c9108f044423b66d3e0fabb359ab6
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-05-07 17:36:40 +1000

    Implementation: Adding an additional bio string for new business profile

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -4,7 +4,8 @@ import SocialLinks from '../PopUpComponents/SocialLinks';
 export default function AboutUs() {
 
   const aboutUsText = "At Ascensionn, we specialize in helping Angel Investors attract over 50 thesis-aligned deals within 180 days. Our unique approach empowers investors to build a consistent and proprietary deal flow, ensuring they find high-quality opportunities that match their specific investment goals. With a focus on precision and efficiency, we streamline the deal-sourcing process to create lasting value for both investors and startups.";
-  const personalBio = "I'm Gurnoor Singh, founder of Ascensionn. My focus is on helping angel investors find 50+ thesis-aligned deals in just 180 days, while building a steady flow of exclusive opportunities. With a background in real estate and a passion for connecting the right investors to the right deals, I’m dedicated to making the investment process smoother and more efficient."
+  const personalBio1 = "I'm Gurnoor Singh, founder of Ascensionn. My focus is on helping angel investors find 50+ thesis-aligned deals in just 180 days, while building a steady flow of exclusive opportunities. With a background in real estate and a passion for connecting the right investors to the right deals, I’m dedicated to making the investment process smoother and more efficient."
+  const personalBio2 = "";
 
   const [popup1, setPopUp1] = useState(false);
   const [popup2, setPopUp2] = useState(false);
@@ -43,7 +44,7 @@ export default function AboutUs() {
                 {/* Personal info */}
                 <div className='w-full items-center font-normal flex flex-col gap-10'>
                   <h1 className='text-lg'>Gurnoor Singh</h1>
-                  <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
+                  <p className='text-center text-md w-[80%] italic'>"{personalBio1}"</p>
                 </div>
                 {/* Acomplishments */}
                 <div></div>
@@ -57,7 +58,7 @@ export default function AboutUs() {
                 {/* Personal info */}
                 <div className='w-full items-center font-normal flex flex-col gap-10'>
                   <h1 className='text-lg'>Unknown Business Partner</h1>
-                  <p className='text-center text-md w-[80%] italic'>"{personalBio}"</p>
+                  <p className='text-center text-md w-[80%] italic'>"{personalBio1}"</p>
                 </div>
                 {/* Acomplishments */}
                 <div></div>
