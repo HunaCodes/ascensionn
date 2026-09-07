b7171f233a5e069124c4b36d47bdd20e9b612d3b
CodeSpecterX <aulakhharnoor11@gmail.com>
2026-09-07 19:30:33 +1000

    Fixing Bio Card sizing

--- src/Components/Pages/AboutUs.tsx
+++ src/Components/Pages/AboutUs.tsx
@@ -40,7 +40,7 @@ export default function AboutUs() {
           {/* Image of the creators */}
           <div className='w-full flex justify-center gap-10'>
             {/* Gurnoor Bio Card*/}
-            <img className="w-1/2 h-1/2 transform hover:scale-90 duration-300 cursor-pointer rounded-4xl" src="/gurnoor_pf.jpeg" onClick={()=>setPopUp1(true)}></img>   
+            <img className="w-[20%] h-[80%] transform hover:scale-90 duration-300 cursor-pointer rounded-4xl" src="/gurnoor_pf.jpeg" onClick={()=>setPopUp1(true)}></img>   
             {popup1 === true && <div className='absolute inset-0 m-auto w-1/2 h-[80%] bg-[#0d3a50] border-2 border-black rounded-4xl z-50'> 
               <button className = "justify-end px-4 cursor-pointer flex w-full" onClick={()=>(setPopUp1(false))}>Close</button>            
                 {/* Personal info */}
